f32cb570b3b26986b9b9bcc78ead5aba0f1b8897
Ian Engelbrecht <ianicus.za@gmail.com>
2025-05-14 22:19:30 +0200

    comments

--- src/routes/[stage]/+page.svelte
+++ src/routes/[stage]/+page.svelte
@@ -3,15 +3,21 @@
   const { data } = $props();
   const { stage } = data;
 
-  const localStage = $state(stage)
+  // trying to pick up the value change here...
+  $effect(() => {
+    console.log('stage changed to', stage);
+  });
+
   const handleButtonClick = (ev: Event) => {
     ev.preventDefault();
-    goto('/' + (localStage == 'new' ? 'old' : 'new'), {
-      invalidateAll: true,
+    goto('/' + (stage == 'new' ? 'old' : 'new'), {
+      invalidateAll: true, // I thought this might work but it doesn't
     });
   };
 </script>
 
-<div>Stage is {localStage}</div>
-<button class="p-2 border rounded cursor-pointer" onclick={handleButtonClick}>Go to {localStage == 'new'? 'old': 'new'}</button>
-<a href={'/' + (localStage == 'new' ? 'old' : 'new')}>Go to {localStage == 'new'? 'old': 'new'}</a>
+<div class="flex flex-col gap-4">
+  <div>Stage is <span class="font-bold">{stage}</span> (this should update)</div>
+  <button class="p-2 w-66 border rounded cursor-pointer" onclick={handleButtonClick}>goto {stage == 'new'? 'old': 'new'} via button click handler</button>
+  <a href={'/' + (stage == 'new' ? 'old' : 'new')}>goto {stage == 'new'? 'old': 'new'} via link</a>
+</div>
