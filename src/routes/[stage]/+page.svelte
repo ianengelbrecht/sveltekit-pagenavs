6fc88c815c751e411f4df6cfbc1f5968cd709017
Ian Engelbrecht <ianicus.za@gmail.com>
2025-05-15 10:30:30 +0200

    added derived value to watch for changes

--- src/routes/[stage]/+page.svelte
+++ src/routes/[stage]/+page.svelte
@@ -3,21 +3,23 @@
   const { data } = $props();
   const { stage } = data;
 
+  let currentStage = $derived(stage);
+
   // trying to pick up the value change here...
   $effect(() => {
-    console.log('stage changed to', stage);
+    console.log('stage changed to', currentStage);
   });
 
   const handleButtonClick = (ev: Event) => {
     ev.preventDefault();
-    goto('/' + (stage == 'new' ? 'old' : 'new'), {
+    goto('/' + (currentStage == 'new' ? 'old' : 'new'), {
       invalidateAll: true, // I thought this might work but it doesn't
     });
   };
 </script>
 
 <div class="flex flex-col gap-4">
-  <div>Stage is <span class="font-bold">{stage}</span> (this should update)</div>
-  <button class="p-2 w-66 border rounded cursor-pointer" onclick={handleButtonClick}>goto {stage == 'new'? 'old': 'new'} via button click handler</button>
-  <a href={'/' + (stage == 'new' ? 'old' : 'new')}>goto {stage == 'new'? 'old': 'new'} via link</a>
+  <div>Stage is <span class="font-bold">{currentStage}</span> (this should update)</div>
+  <button class="p-2 w-66 border rounded cursor-pointer" onclick={handleButtonClick}>goto {currentStage == 'new'? 'old': 'new'} via button click handler</button>
+  <a href={'/' + (currentStage == 'new' ? 'old' : 'new')}>goto {currentStage == 'new'? 'old': 'new'} via link</a>
 </div>
